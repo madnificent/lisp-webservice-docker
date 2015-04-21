51c30d941db10c8af7e0a7a70a7293a977c61854
Aad Versteden <madnificent@gmail.com>
2015-04-21 21:46:36 +0200

    Add support for load command
    Dockers basing themselves on this docker can preload the necessary
    quicklisp libraries with relative ease now.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM davazp/quicksbcl
 
-RUN apt-get update; apt-get upgrade -y; apt-get install -y openssl
+RUN apt-get update; apt-get upgrade -y; apt-get install -y openssl;
 
-ADD ./startup.lisp /tmp/startup.lisp
+ADD ./startup.lisp /usr/src/startup.lisp
+ADD ./load.lisp /usr/src/load.lisp
 
-CMD sbcl --load /tmp/startup.lisp
+CMD sbcl --load /usr/src/startup.lisp
+
 EXPOSE 4005
 EXPOSE 80
---
+++ load.lisp
@@ -0,0 +1,36 @@
+;; call this to load everything we know with the current
+;; environment variables, so we can already store it in
+;; the image.
+(defun env-value (setting)
+  "Returns the value of the supplied environment variable."
+  (sb-ext:posix-getenv (string-upcase (string setting))))
+
+(defun feature-setting-p (setting)
+  "Returns non-nil iff the setting is enabled."
+  (let ((value (env-value (concatenate 'string "HAS_" (string-upcase (string setting))))))
+    (and value (not (equal value "")))))
+
+(format t "~& >> docker setting up environment ... ~%")
+(push :docker *features*)
+(push #p"/app/" ql:*local-project-directories*)
+(format t "~& >> docker finished setting up environment~%")
+
+;;; do we have base systems to load?
+(format t "~& >> docker loading base systems ... ~%")
+(ql:quickload :split-sequence)
+(when (env-value :systems)
+  (let ((systems (split-sequence:split-sequence #\Space (env-value :systems))))
+    (ql:quickload systems)))
+(format t "~& >> docker finished loading base systems ~%")
+
+;;; do we have a bootable system to load?
+(format t "~& >> docker loading boot ... ~%")
+(when (env-value :boot)
+  (let ((name (string-upcase (env-value :boot))))
+    (ql:quickload name)))
+(format t "~& >> docker finished loading boot ~%")
+
+;;; getting swank up
+(format t "~& >> docker loading swank ... ~%")
+(ql:quickload :swank) ;; we need this toplevel
+(format t "~& >> docker finished loading swank ~%")
